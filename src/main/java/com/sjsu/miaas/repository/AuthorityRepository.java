b621cb9c448fa43d19b91e857b41754d4099c4d0
simpleclicks <sheetalparanjpe@gmail.com>
2014-11-01 20:54:56 -0700

    get amazon instance by id

--- src/main/java/com/sjsu/miaas/repository/AuthorityRepository.java
+++ src/main/java/com/sjsu/miaas/repository/AuthorityRepository.java
@@ -1,10 +1,20 @@
 package com.sjsu.miaas.repository;
 
+import java.util.List;
+
+import com.sjsu.miaas.domain.AmazonInstance;
 import com.sjsu.miaas.domain.Authority;
+import com.sjsu.miaas.domain.Request;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Spring Data JPA repository for the Authority entity.
  */
 public interface AuthorityRepository extends JpaRepository<Authority, String> {
+	
+	@Query("select a from AmazonInstance a where a.instanceId = ?1")
+    AmazonInstance getAmazonInstancebyId(String instanceId);
+	
 }
