89ef4a6eb9f38f195f5df2c095e974ea3e5b6cad
Ameya Koshti <akoshti@paypal.com>
2014-11-06 00:19:26 -0800

    billing added

--- src/main/webapp/scripts/request/controller_request.js
+++ src/main/webapp/scripts/request/controller_request.js
@@ -1,6 +1,7 @@
 'use strict';
 
 miaasApp.controller('RequestController', function ($rootScope, $scope, resolvedRequest, Request, UserRequest, RequestDevices) {
+        var resourcePricePerDay = 5;
 
         $scope.preDefRequestType = [
             { value: 'Android', text: 'Android' },
@@ -30,12 +31,14 @@ miaasApp.controller('RequestController', function ($rootScope, $scope, resolvedR
         $scope.requests = resolvedRequest;
         //$scope.users = resolvedUser;
         $scope.users = $rootScope.account.login;
+
         $scope.create = function () {
             var userobj = {
                 login : $scope.users
             };
             $scope.request.user = userobj;
-            console.log($scope.request);
+            //console.log($scope.request);
+            //$scope.request['resourcePrice'] = calculatePrice($scope.request.startDate, $scope.request.endDate, resourcePricePerDay);
             Request.save($scope.request,
                 function () {
                     $scope.requests = UserRequest.query();
@@ -56,7 +59,15 @@ miaasApp.controller('RequestController', function ($rootScope, $scope, resolvedR
                 });
         };
 
+        $scope.calculatePrice = function(startDate, endDate, rate) {
+            var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+            var firstDate = new Date(endDate);
+            var secondDate = new Date(startDate);
 
+            var days = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+
+            return days*rate;
+        };
 
         $scope.deviceView = function(req){
             $scope.selectedReqId = req.id;
