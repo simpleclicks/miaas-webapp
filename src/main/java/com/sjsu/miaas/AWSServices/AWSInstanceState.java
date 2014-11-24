229186bb328b348bd6b27ba4ba85e95439572904
sourabhbhosale <sourabh.bhosale@sjsu.edu>
2014-11-24 13:39:23 -0800

    NetworkIn and NetworkOut added to AWSMetric -- again

--- src/main/java/com/sjsu/miaas/AWSServices/AWSInstanceState.java
+++ src/main/java/com/sjsu/miaas/AWSServices/AWSInstanceState.java
@@ -40,8 +40,8 @@ public class AWSInstanceState {
 		// return new PropertiesCredentials(
 		// AWSInstanceState.class.getResourceAsStream("credentials"));
 		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
-				"AKIAJWCCXYK6FOWWFMQQ",
-				"44n9wOw1nRP09xlGj9ZzjEp7+nBTiXNc4fm8PB9I");
+				"aaaa",
+				"aaaa");
 
 		return awsCredentials;
 	}
