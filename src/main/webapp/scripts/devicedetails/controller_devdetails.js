082fbc8d7ad16286680b18499551d5962fc1c73a
simpleclicks <sheetalparanjpe@gmail.com>
2014-12-04 13:00:33 -0800

    app name dynamically sent

--- src/main/webapp/scripts/devicedetails/controller_devdetails.js
+++ src/main/webapp/scripts/devicedetails/controller_devdetails.js
@@ -7,34 +7,39 @@
 miaasApp.controller('DevDetailsController', function ($rootScope, $scope, DeviceDetails) {
     $scope.app = {};
     var device = DeviceDetails.get();
-    if(device.deviceVersion == 19) {
+    if (device.deviceVersion == 19) {
         device.deviceOS = "Android 4.4";
         device.deviceOSName = "KitKat";
     }
-    else if(device.deviceVersion == 18) {
+    else if (device.deviceVersion == 18) {
         device.deviceOS = "Android 4.3";
         device.deviceOSName = "JellyBean";
     }
-    else if(device.deviceVersion == 21) {
+    else if (device.deviceVersion == 21) {
         device.deviceOS = "Android 5.0";
         device.deviceOSName = "Lollipop";
     }
     $scope.selectedDevice = device;
     console.log($scope.selectedDevice);
 
+    $scope.getTartgetUrl = function(){
+        return "http://" + $scope.selectedDevice.amazonInstance.publicDnsName +
+            "/simpleapp/webapi/androidcontrol/install/" +
+            $scope.app.name + "/" + $scope.selectedDevice.deviceId;
+    };
 
     $scope.uploader = new Flow({
-        target : "http://" + $scope.selectedDevice.amazonInstance.publicDnsName + "/simpleapp/webapi/androidcontrol/install/appname/id",
-        query : {
-            appName : $scope.app.name,
-            appDesc : "desc"
-        },
-        fileParameterName : "apkfile",
-        testMethod : "POST"
+        target: "http://" + $scope.selectedDevice.amazonInstance.publicDnsName +
+            "/simpleapp/webapi/androidcontrol/install/" +
+            $scope.app.name + "/" + $scope.selectedDevice.deviceId,
+        fileParameterName: "apkfile",
+        testMethod: "POST"
     });
 
 
-    $scope.uploadFiles = function() {
+
+    $scope.uploadFiles = function () {
+        $scope.uploader.opts.target = $scope.getTartgetUrl();
         $scope.uploader.upload();
     }
 
